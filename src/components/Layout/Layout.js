012a4733194ce6bf5d24701629ebdf3484b3780e
= <=>
2019-12-02 17:23:35 +0400

    BurgerBuilder container was implemented...

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Auxiliary from '../hoc/Auxiliary';
+import classes from './Layout.module.css';
+
+const Layout = props => (
+    <Auxiliary>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Auxiliary>
+);
+
+export default Layout;
