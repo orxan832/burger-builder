0699c42e674ac7fa17e4ba1e340c2c78c748ca2f
orxan832 <orxan832@gmail.com>
2019-12-03 22:07:25 +0400

    Continued

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const Toolbar = props => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo/>
-        <nav>...</nav>
+        <div className={classes.Logo}><Logo/></div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems/>
+        </nav>
     </header>
 );
 
