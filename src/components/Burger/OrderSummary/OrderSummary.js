53a320b83066c19432b89b632b67f01217d52e61
= <=>
2019-12-04 12:10:53 +0400

    The Burger Builder project first round is finished

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
+import React, { Component } from 'react';
+import Auxiliary from '../../../containers/hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return <li key={igKey}>{igKey}: {props.ingredients[igKey]}</li>
-        });
-    return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Total price: <strong>{props.totalPrice.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-        </Auxiliary>
-    );
+class OrderSummary extends Component {
+    //This class could be a functional component, doesn't have to be a class component
+    componentDidUpdate(){
+        console.log('[OrderSummary] is rendering');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}>{igKey}: {this.props.ingredients[igKey]}</li>
+            });
+        return (
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Total price: <strong>{this.props.totalPrice.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </Auxiliary>
+        );
+    }
 }
 
 export default OrderSummary;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,23 @@
 import React from "react";
-
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.module.css";
+import Auxiliary from '../../../containers/hoc/Auxiliary/Auxiliary';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const SideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  attachedClasses = props.open ? [classes.SideDrawer, classes.Open] : attachedClasses;
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}><Logo /></div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Auxiliary>
+      <Backdrop show clicked={props.closed} show={props.open}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}><Logo /></div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Auxiliary>
   );
 };
 
